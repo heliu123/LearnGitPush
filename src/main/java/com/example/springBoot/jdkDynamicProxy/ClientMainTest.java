3129aaa877764d1e9755e10dff571ff380fa27ed
heliu <heliu-yf@log56.com>
2019-04-15 16:33:51 +0800

    文件重命名

--- src/main/java/com/example/springBoot/jdkDynamicProxy/ClientMainTest.java
+++ src/main/java/com/example/springBoot/jdkDynamicProxy/ClientMainTest.java
@@ -4,7 +4,7 @@ package com.example.springBoot.jdkDynamicProxy;
  * client测试代码
  * Created by Administrator on 2019/4/3.
  */
-public class Client {
+public class ClientMainTest {
     public static void main(String[] args) {
         // 保存生成的代理类的字节码文件
         //System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
